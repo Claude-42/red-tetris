459b7cbbf34d8fae24bb780acf23e305df4ead71
Devessier <baptiste@devessier.fr>
2020-11-13 17:26:49 +0100

    style: format test

--- server/test/piece.spec.js
+++ server/test/piece.spec.js
@@ -2,18 +2,28 @@ const { Piece, PIECE_TYPE, PIECES } = require('../src/piece')
 
 describe('The Piece class', () => {
   test('normalize method works for RED piece', () => {
-      const piece = new Piece(PIECES[PIECE_TYPE.RED])
+    const piece = new Piece(PIECES[PIECE_TYPE.RED])
 
-      piece.normalize()
+    piece.normalize()
 
-      expect(piece.coordinateList).toStrictEqual([{x:0, y:0}, {x:1, y:0}, {x:1, y:1}, {x:2, y:1}])
+    expect(piece.coordinateList).toStrictEqual([
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+    ])
   })
 
   test('normalize method works for PURPLE piece', () => {
-      const piece = new Piece(PIECES[PIECE_TYPE.PURPLE])
+    const piece = new Piece(PIECES[PIECE_TYPE.PURPLE])
 
-      piece.normalize()
+    piece.normalize()
 
-      expect(piece.coordinateList).toStrictEqual([{x:1, y:0}, {x:0, y:1}, {x:1, y:1}, {x:2, y:1}])
+    expect(piece.coordinateList).toStrictEqual([
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+    ])
   })
 })
