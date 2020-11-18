6b1979186f153e7c953de4b3f4472d1515c3aa00
Devessier <baptiste@devessier.fr>
2020-11-18 18:09:27 +0100

    feat: add useService composable from @xstate/vue library

--- client/src/composables/machine.js
+++ client/src/composables/machine.js
@@ -1,4 +1,9 @@
-import { shallowRef, onMounted, onBeforeUnmount } from "vue";
+/**
+ * Code imported from https://github.com/davidkpiano/xstate/blob/master/packages/xstate-vue/src/index.ts
+ * We could not use @xstate/vue library because they use @vue/composition-api when we use Vue 3 directly.
+ */
+
+import { shallowRef, onMounted, onBeforeUnmount, isRef, watch } from "vue";
 import { interpret, State } from "xstate";
 
 export function useMachine(machine, options = {}) {
@@ -48,4 +53,29 @@ export function useMachine(machine, options = {}) {
   });
 
   return { state, send: service.send, service };
+}
+
+export function useService(service) {
+  const serviceRef = isRef(service) ? service : shallowRef(service);
+  const state = shallowRef(serviceRef.value.state);
+
+  watch(
+    serviceRef,
+    (service, _, onCleanup) => {
+      state.value = service.state;
+      const { unsubscribe } = service.subscribe(currentState => {
+        if (currentState.changed) {
+          state.value = currentState;
+        }
+      });
+      onCleanup(() => unsubscribe());
+    },
+    {
+      immediate: true
+    }
+  );
+
+  const send = event => serviceRef.value.send(event);
+
+  return { state, send, service: serviceRef };
 }
