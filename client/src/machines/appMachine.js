0789243f72f23bed37b0e9bf413ef1f1483dc3c0
Devessier <baptiste@devessier.fr>
2020-11-20 16:10:40 +0100

    feat: create a state machine to handle application and game states
    This state machine also has a service that communicates
    to socket.io server.
    Moves are correctly sent to the server through socket.io.
    Each second a `DOWN` event is sent to the server.
    Events are debounced to prevent flood.

---
+++ client/src/machines/appMachine.js
@@ -0,0 +1,158 @@
+import { assign, forwardTo, Machine, send } from "xstate";
+import io from "socket.io-client";
+
+const WEBSOCKET_URL = "http://localhost:3030";
+const GO_DOWN_TIME = 1000;
+const DEBOUNCE_TIME = 200;
+
+export const appMachine = Machine(
+  {
+    id: "app",
+    context: {
+      grid: undefined,
+      nextPiece: undefined
+    },
+    invoke: {
+      id: "websocket",
+      src: "setupWebsocket"
+    },
+    initial: "initializing",
+    states: {
+      initializing: {
+        on: {
+          SOCKET_CONNECTED: "waiting"
+        }
+      },
+      waiting: {
+        on: {
+          START_GAME: {
+            target: "playing",
+            actions: "sendStartGameToWebsocket"
+          }
+        }
+      },
+      playing: {
+        type: "parallel",
+        states: {
+          moving: {
+            initial: "idle",
+            states: {
+              idle: {
+                on: {
+                  "MOVE.ROTATE": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  },
+                  "MOVE.LEFT": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  },
+                  "MOVE.RIGHT": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  },
+                  "MOVE.DOWN": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  },
+                  "MOVE.DOWN_AUTOMATIC": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  },
+                  "MOVE.FALL": {
+                    target: "debounce",
+                    actions: "forwardToWebsocket"
+                  }
+                }
+              },
+              debounce: {
+                after: {
+                  [DEBOUNCE_TIME]: "idle"
+                }
+              }
+            }
+          },
+          moveDownTimer: {
+            initial: "idle",
+            states: {
+              idle: {
+                after: {
+                  [GO_DOWN_TIME]: "inc"
+                }
+              },
+              inc: {
+                entry: "sendAutomaticDownToWebsocket",
+                on: {
+                  "": "idle"
+                }
+              }
+            }
+          }
+        },
+        on: {
+          UPDATE_GRID_DATA: {
+            actions: assign({
+              grid: (_context, { payload: { PAINT_GRID } }) => PAINT_GRID,
+              nextPiece: ({ NEXT_PIECE }) => NEXT_PIECE
+            })
+          }
+        }
+      }
+    }
+  },
+  {
+    services: {
+      setupWebsocket: () => (callback, onReceive) => {
+        const socket = io(WEBSOCKET_URL);
+
+        socket.on("connect", () => {
+          callback("SOCKET_CONNECTED");
+        });
+
+        socket.on("ownGrid", payload => {
+          callback({
+            type: "UPDATE_GRID_DATA",
+            payload
+          });
+        });
+
+        onReceive(event => {
+          switch (event.type) {
+            case "START_GAME":
+              socket.emit("start");
+              break;
+            case "MOVE.ROTATE":
+              socket.emit("move", { type: "ROTATE" });
+              break;
+            case "MOVE.DOWN":
+              socket.emit("move", { type: "DOWN" });
+              break;
+            case "MOVE.DOWN_AUTOMATIC":
+              socket.emit("move", { type: "DOWN" });
+              break;
+            case "MOVE.LEFT":
+              socket.emit("move", { type: "LEFT" });
+              break;
+            case "MOVE.RIGHT":
+              socket.emit("move", { type: "RIGHT" });
+              break;
+            case "MOVE.FALL":
+              socket.emit("move", { type: "FALL" });
+              break;
+          }
+        });
+
+        return () => {
+          socket.disconnect();
+        };
+      }
+    },
+    actions: {
+      sendStartGameToWebsocket: send("START_GAME", { to: "websocket" }),
+      forwardToWebsocket: forwardTo("websocket"),
+      sendAutomaticDownToWebsocket: send("MOVE.DOWN_AUTOMATIC", {
+        to: "websocket"
+      })
+    }
+  }
+);
