8f42947dd1cd95ab97ae3bb3c5fa2387f472bcee
Devessier <baptiste@devessier.fr>
2020-11-20 16:11:48 +0100

    feat: make application state machine available throughout components
    We do it by using provide/inject composition api functions :tada:!

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,9 +7,15 @@
 <script>
 import TheHeader from "./components/TheHeader.vue";
 
+import { useProvideAppMachine } from "./composables/app.js";
+
 export default {
   components: {
     TheHeader
+  },
+
+  setup() {
+    useProvideAppMachine();
   }
 };
 </script>
---
+++ client/src/composables/app.js
@@ -0,0 +1,22 @@
+import { inject, provide } from "vue";
+
+import { appMachine } from "../machines/appMachine.js";
+import { useMachine } from "../composables/machine.js";
+
+export const AppMachineSymbol = Symbol("app-machine");
+
+export function useProvideAppMachine() {
+  const appMachineContext = useMachine(appMachine);
+
+  provide(AppMachineSymbol, appMachineContext);
+}
+
+export function useAppMachine() {
+  const { state, send, service } = inject(AppMachineSymbol);
+
+  return {
+    appMachineState: state,
+    appMachineSend: send,
+    appMachineService: service
+  };
+}
