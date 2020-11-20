1261d882f7f7f63ba19cf20893fcc31d13c5f403
Devessier <baptiste@devessier.fr>
2020-11-20 16:08:16 +0100

    refactor: remove vue-socket.io use

--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { VueSocketPlugin } from "./plugins/socket";
 
 import "./assets/index.css";
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueSocketPlugin, {
-    connection: "http://localhost:3030"
-  })
   .mount("#app");
