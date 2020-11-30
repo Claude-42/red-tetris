0bfa99314e44a37cf7057e680dc8ead5ad243d16
Devessier <baptiste@devessier.fr>
2020-11-30 17:45:02 +0100

    feat(client): add an alt text when there is no other player

--- client/src/components/TheBoardGameShadowList.vue
+++ client/src/components/TheBoardGameShadowList.vue
@@ -1,16 +1,32 @@
+<i18n lang="yaml">
+en:
+  no-other-players: "There is no other player"
+
+fr:
+  no-other-players: "Vous êtes le seul joueur"
+</i18n>
+
 <template>
-  <div class="flex flex-wrap w-full">
-    <BoardGameShadowContainer
-      v-for="{ name: playerName, grid } in players"
-      :key="playerName"
-      :player-name="playerName"
-      :grid="grid"
-      class="p-1"
-    />
+  <div class="w-full">
+    <div v-if="players?.length > 0" class="flex flex-wrap">
+      <BoardGameShadowContainer
+        v-for="{ name: playerName, grid } in players"
+        :key="playerName"
+        :player-name="playerName"
+        :grid="grid"
+        class="p-1"
+      />
+    </div>
+
+    <p v-else class="text-sm text-center text-gray-600">
+      {{ t("no-other-players") }}
+    </p>
   </div>
 </template>
 
 <script>
+import { useI18n } from "vue-i18n";
+
 import BoardGameShadowContainer from "./BoardGameShadowContainer.vue";
 
 export default {
@@ -23,6 +39,14 @@ export default {
       type: Array,
       required: true,
     },
+  },
+
+  setup() {
+    const { t } = useI18n();
+
+    return {
+      t,
+    };
   },
 };
 </script>
