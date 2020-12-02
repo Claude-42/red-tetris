4bab50b55efa98aeb996e7cc0989e4f69b047ef1
Devessier <baptiste@devessier.fr>
2020-12-02 18:21:50 +0100

    build(client): comply to dumb subject restrictions
    CSS must be inlined and all JS must be compiled in a single file.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,10 +1,37 @@
 module.exports = {
   chainWebpack: (config) => {
+    /**
+     * Output JS to bundle.js file
+     */
+    config.output.filename("bundle.js");
+
+    /**
+     * Disable chunks splitting
+     *
+     * See https://stackoverflow.com/a/56272461/11780994
+     */
+    config.optimization.splitChunks(undefined);
+
     config.module
       .rule("i18n")
       .resourceQuery(/blockType=i18n/)
       .type("javascript/auto")
       .use("i18n")
       .loader("@intlify/vue-i18n-loader");
+
+    /**
+     * Inline CSS
+     */
+    config.plugin("preload").tap((args) => {
+      args[0].fileBlacklist.push(/\.css/, /app\.js/);
+      return args;
+    });
+    config
+      .plugin("inline-source")
+      .use(require("html-webpack-inline-source-plugin"));
+    config.plugin("html").tap((args) => {
+      args[0].inlineSource = "(.css|app.js$)";
+      return args;
+    });
   },
 };
