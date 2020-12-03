9ee9a59cce5271b9faf064420479d707d51e4a64
Devessier <baptiste@devessier.fr>
2020-12-03 16:33:02 +0100

    build: modify ls command to debug heroku deployment

--- setup-front.sh
+++ setup-front.sh
@@ -7,7 +7,8 @@ npm run build
 
 cd ..
 
-ls # for testing purpose
+ls client # for testing purpose
+ls -R client/dist # for testing purpose
 
 cp client/dist/index.html server/public
 cp client/dist/bundle.js server/public
