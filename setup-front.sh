b203064bad2f002dafa00b18d277d3bc0666b864
Devessier <baptiste@devessier.fr>
2020-12-06 16:17:47 +0100

    build: install dependencies after build and setup front and back when starting the app

--- setup-front.sh
+++ setup-front.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
 cd client
-
 npm run build
 
-
 cd ..
-
-ls client # for testing purpose
-ls -R client/dist # for testing purpose
-
 cp client/dist/index.html server/public
 cp client/dist/bundle.js server/public
--- start-production.sh
+++ start-production.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+(./setup-front.sh)
+(./setup-back.sh)
+
 NODE_ENV=production cd server && npm start
